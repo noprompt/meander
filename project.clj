2d7242051d539ec9c291f5e1398b9a6e97fabfbb
Joel Holdbrooks <cjholdbrooks@gmail.com>
2018-01-27 00:22:21 -0800

    Update project dependencies, fix typo

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.logic "0.8.11"]
-                 [com.taoensso/tufte "1.1.2"]]
-
-  :profiles {:dev {:dependences [[org.clojure/tools.nrepl "0.2.10"]
-                                 [fipp "0.6.10"]]}})
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/math.combinatorics "0.1.4"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [com.taoensso/tufte "1.1.2"]
+                                  [fipp "0.6.10"]]}})
