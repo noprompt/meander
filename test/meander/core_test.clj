182e1f84b26b79c6f9641d325c8db2937d9f6b5c
Joel Holdbrooks <cjholdbrooks@gmail.com>
2018-02-12 22:27:24 -0800

    Start testing

--- test/meander/core_test.clj
+++ test/meander/core_test.clj
@@ -1,7 +1,43 @@
 (ns meander.core-test
-  (:require [clojure.test :refer :all]
-            [meander.core :refer :all]))
+  (:require [clojure.test :as t]
+            [meander.core :as r]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; ---------------------------------------------------------------------
+;; t-test
+
+(def dot-form-t1
+  ;; (.foo bar baz)
+  (r/t (~method ~target ~@args)
+    :when (symbol? method)
+    :let [method-name (name method)]
+    :when (re-find #"\A\.." method-name)
+    :let [method* (symbol (subs method-name 1))]
+    (. ~target ~method* ~@args)))
+
+
+(def dot-form-t2
+  ;; (. bar (foo baz))
+  (r/t (. ~target (~method ~@args))
+    :when (symbol? method)
+    (. ~target ~method ~@args)))
+
+(t/deftest t-test
+  (t/testing "transforms implement IFn"
+    (t/is (= '(. bar foo baz)
+             (dot-form-t1 '(.foo bar baz)))))
+
+  (t/testing "transforms are unifiable"
+    (t/is (= '({"args" (baz)
+                "method" .foo
+                "target" bar})
+             (r/unify* dot-form-t1 '(.foo bar baz) {})))
+
+    (t/is (= '()
+             (r/unify* dot-form-t1 '(. bar (foo baz)) {})))))
+
+
+
+
+
+
+
