345e1722ab2ed1d5649b3f8323789f55b64b4967
Joel Holdbrooks <cjholdbrooks@gmail.com>
2018-10-06 18:24:19 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject meander "0.1.0-SNAPSHOT"
+(defproject meander/meander.alpha "0.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/math.combinatorics "0.1.4"]
-                                  [org.clojure/tools.nrepl "0.2.10"]
-                                  [org.clojure/test.check "0.10.0-alpha2"]
-                                  [com.taoensso/tufte "1.1.2"]
-                                  [fipp "0.6.10"]]
-                   :source-paths ["src" #_"dev"]}})
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                                  [org.clojure/test.check "0.10.0-alpha2"]]
+                   :source-paths ["src"]}})
