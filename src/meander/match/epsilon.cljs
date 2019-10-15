1727a50b1145dca3215be17a792716a3d1473878
Joel Holdbrooks <cjholdbrooks@gmail.com>
2019-10-14 22:24:08 -0700

    Split match ns into clj/cljs files

---
+++ src/meander/match/epsilon.cljs
@@ -0,0 +1,5 @@
+(ns ^:no-doc meander.match.epsilon
+  (:require [goog.object :as gobj]
+            [meander.match.runtime.epsilon :as r.match.runtime]
+            [meander.match.specs.epsilon])
+  (:require-macros [meander.match.epsilon]))
