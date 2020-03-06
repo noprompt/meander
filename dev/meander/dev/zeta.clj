25eb75f9bb4fe678d84f7efa08109c86657cd306
Joel Holdbrooks <cjholdbrooks@gmail.com>
2020-03-06 00:23:52 -0800

    Use meander.syntax.zeta in dev

--- dev/meander/dev/zeta.clj
+++ dev/meander/dev/zeta.clj
@@ -3,27 +3,10 @@
             [meander.epsilon :as me]
             [meander.zeta :as mz]
             [meander.runtime.zeta :as m.runtime]
+            [meander.syntax.zeta :as m.syntax]
             [meander.dev.parse.zeta :as dev.parse]
             [meander.dev.match.zeta :as dev.match]
             [meander.dev.subst.zeta :as dev.subst]))
-
-(defn ns-symbolic-alias-map [ns]
-  (into {} (map (fn [[alias ns]]
-                  [alias (ns-name ns)]))
-        (ns-aliases ns)))
-
-(defn make-env []
-  {:aliases (ns-symbolic-alias-map *ns*)
-   :cata-symbol (gensym "C__")
-   :state-symbol (gensym "S__")})
-
-(defn parse-pattern
-  ([pattern]
-   (parse-pattern pattern (make-env)))
-  ([pattern env]
-   (let [ast (dev.parse/parse [pattern env])
-         ast {:tag :root :next ast}]
-     ast)))
 
 (defn variables [ast]
   (me/search ast
@@ -32,9 +15,8 @@
     ?ast))
 
 (defmacro solve [expression pattern]
-  (let [;; Evolve this structure.
-        env (make-env)
-        ast (parse-pattern pattern env)
+  (let [env (m.syntax/make-parse-env *ns*)
+        ast (m.syntax/parse pattern env)
         target (gensym "target__")
         match-form (dev.match/match-compile [(list [ast target]) (merge env (meta &form))])]
     `(let [~target ~expression]
@@ -43,12 +25,12 @@
 (defmacro search
   {:style/indent 1}
   [expression & clauses]
-  (let [env (make-env)
+  (let [env (m.syntax/make-parse-env *ns*)
         target (gensym "target___")]
     `(let [~target ~expression]
        ~@(map
           (fn [[left right]]
-            (let [left-ast (parse-pattern left env)
+            (let [left-ast (m.syntax/parse left env)
                   match-form (dev.match/match-compile [(list [left-ast target]) env])
                   syms (into [] (comp (map :symbol) (distinct)) (variables left-ast))
                   state (gensym "S__")]
@@ -68,12 +50,12 @@
 (defmacro rewrite-1
   {:style/indent 1}
   [expression match-pattern subst-pattern]
-  (let [env (make-env)
-        match-ast (parse-pattern match-pattern env)
+  (let [env (m.syntax/make-parse-env)
+        match-ast (m.syntax/parse match-pattern env)
         cata-symbol (get env :cata-symbol)
         target (gensym "target__")
         match-form (dev.match/match-compile [(list [match-ast target]) env])
-        subst-ast (parse-pattern subst-pattern env)
+        subst-ast (m.syntax/parse subst-pattern env)
         generator (gensym "generator__")
         subst-form (dev.subst/generate-compile subst-ast)]
     `(let [~target ~expression
@@ -87,7 +69,7 @@
 
 (defn rewrites-code
   [expression clauses]
-  (let [env (make-env)
+  (let [env (m.syntax/make-parse-env *ns*)
         cata-symbol (get env :cata-symbol)
         target-symbol (gensym "T__")
         generator-symbol (gensym)
@@ -98,11 +80,11 @@
              ~cata-symbol
              (`fn ~cata-symbol [~target-symbol]
               (`m.runtime/knit
-               [(`let [~generator-symbol (me/app dev.subst/generate-compile [(me/app parse-pattern !right ~env) ~env])]
+               [(`let [~generator-symbol (me/app dev.subst/generate-compile [(me/app m.syntax/parse !right ~env) ~env])]
                  (`mapcat
                   (`fn [~env-symbol]
                    (`m.runtime/run-gen ~generator-symbol ~env-symbol))
-                  (me/app dev.match/match-compile [([(me/app parse-pattern !left ~env) ~target-symbol]) ~env])))
+                  (me/app dev.match/match-compile [([(me/app m.syntax/parse !left ~env) ~target-symbol]) ~env])))
                 ...]))]
        (~cata-symbol ~target-symbol)))))
 
@@ -139,8 +121,8 @@
         :search-answer ~search-form})))
 
 (defmacro gen [pattern]
-  (let [env (make-env)
-        ast (parse-pattern pattern env)
+  (let [env (m.syntax/make-parse-env)
+        ast (m.syntax/parse pattern env)
         gen (dev.subst/generate-compile [ast env])]
     `(let [gen# ~gen]
        (fn
@@ -148,3 +130,10 @@
           (m.runtime/run-gen gen# env#))
          ([env# n#]
           (m.runtime/run-gen gen# env# n#))))))
+
+(m.syntax/parse
+ '(mz/string mz/& ?bar mz/& ?foo))
+
+(solve "foofoo" (mz/string mz/& ?foo mz/& ?bar))
+
+
