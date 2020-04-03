333b9957cb7b9dc7cab66ef565fe8bb3efb5c9dc
Joel Holdbrooks <cjholdbrooks@gmail.com>
2020-04-02 22:48:49 -0700

    Add place holder vars in meander.zeta

--- src/meander/zeta.clj
+++ src/meander/zeta.clj
@@ -1,1 +1,38 @@
-(ns meander.zeta)
+(ns meander.zeta
+  (:refer-clojure :exclude [or and])
+  (:require [meander.syntax.zeta :as m.syntax]))
+
+(def
+  ^{:arglists '([] [pattern] [pattern-a pattern-b & more-patterns])}
+  and)
+
+(def
+  ^{:arglists '([*mutable-variable initial-value-expr reduce-fn-expr])
+    :style/indent 1}
+  fold)
+
+(def
+  ^{:arglists '([bindings] [bindings body-pattern])
+    :style/indent 1}
+  let)
+
+(def
+  ^{:arglists '([pred-expr & more-patterns])}
+  pred)
+
+(def
+  ^{:arglists '([bindings body-pattern])
+    :style/indent 1}
+  with)
+
+(def
+  ^{:arglists '([] [pattern] [pattern-a pattern-b & more-patterns])}
+  or)
+
+(def
+  ^{:arglists '([pattern])}
+  recur)
+
+(def
+  ^{:arglists '([& patterns])}
+  string)
