3fc3869f73b5712a9545988fddeaf578fead5225
Ben Sless <ben.sless@gmail.com>
2022-04-14 10:35:07 +0300

    Add tests for goal compiler

--- dev/meander/bsless/zeta_kanren.clj
+++ dev/meander/bsless/zeta_kanren.clj
@@ -1,7 +1,8 @@
 (ns meander.bsless.zeta-kanren
   (:require
+   [clojure.test :as t]
    [meander.primitive.zeta :as z]
-   [meander.bsless.ukanren :refer [-disj -conj === call-fresh ->LVar]])
+   [meander.bsless.ukanren :refer [-disj -conj === ->LVar]])
   (:import
    (meander.primitive.zeta Each Some Is Anything)))
 
@@ -20,6 +21,31 @@
   Anything
   (-into-goal [this ctx] (=== true true)))
 
-((-into-goal (z/each (z/some (z/is 1) (z/is 2))
-                     (z/is 2))
-             {:lvar (->LVar 'x)}) {})
+(t/deftest is
+  (t/testing "Should always succeed"
+    (let [x (->LVar 'x)]
+      (t/is (= [{x 1}] ((-into-goal (z/is 1) {:lvar x}) {}))))))
+
+(t/deftest disjunction
+  (t/testing "Should return one or the other"
+    (let [x (->LVar 'x)]
+      (t/is (= [{x 1} {x 2}]
+               ((-into-goal (z/some (z/is 1) (z/is 2)) {:lvar x}) {}))))))
+
+(t/deftest conjunction
+  (t/testing "Conjunction branch eliminates other option"
+    (let [x (->LVar 'x)
+          expr (z/each (z/some (z/is 1) (z/is 2)) (z/is 2))
+          goal (-into-goal expr {:lvar x})]
+      (t/is (= [{x 2}] (goal {}))))))
+
+(comment
+  (defn ==k->v
+    [m k v]
+    (fn [s] (=== (get (val (meander.bsless.ukanren/-lookup s (->LVar m))) k)
+                (->LVar v))))
+
+
+  ((-conj (-into-goal (z/is {:x 1}) {:lvar '?root}) (==k->v '?root :x '?x)) {})
+
+  ((-into-goal (z/is {:x 1}) {:lvar '?root}) {}))
