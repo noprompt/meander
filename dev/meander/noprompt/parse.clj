34b24ccf7a31a907e5397e234fe0e2b86ef8ad7e
Joel Holdbrooks <cjholdbrooks@gmail.com>
2022-07-08 09:28:20 -0700

    WIP

--- dev/meander/noprompt/parse.clj
+++ dev/meander/noprompt/parse.clj
@@ -1,7 +1,8 @@
 (ns meander.noprompt.parse
   (:require [clojure.string :as string]
             [meander.environment.zeta :as m.env]
-            [meander.primitive.zeta :as m.prim]))
+            [meander.primitive.zeta :as m.prim]
+            [meander.primitive.hash-set.zeta :as m.prim.hash-set]))
 
 (defmacro todo [env form]
   `(throw (ex-info "TODO" {:env ~env, :form ~form, :meta '~(meta &form)})))
@@ -33,6 +34,9 @@
       (if (namespace form)
         (m.prim/? form)
         (m.prim/? (symbol (name (::m.env/namespace env)) symbol-name)))
+
+      (= symbol-name "_")
+      (m.prim/anything)
 
       :else
       (parse-literal-symbol env form))))
@@ -86,40 +90,117 @@
   {:pre [(vector? form)]}
   (m.prim/vec (parse-concat env form)))
 
+(defn parse-map [env form]
+  {:pre [(map? form)]}
+  (reduce
+   (fn [m [k v]]
+     (m.prim/assoc m (parse env k) (parse env v)))
+   (m.prim/hash-map)
+   form))
+
+(defn parse-set [env form]
+  {:pre [(set? form)]}
+  (apply m.prim/hash-set (parse-all env form)))
+
+;; NOTE: Experimental
+(defn apply-extensions [env form]
+  (reduce
+   (fn [form [k f]]
+     (let [form (if (instance? clojure.lang.IObj form)
+                  (vary-meta form assoc ::m.env/env env)
+                  form)]
+       (if-some [{:keys [object]} (f form)]
+         (reduced object)
+         form)))
+   form
+   (::m.env/extensions env)))
+
 (defn parse [env form]
-  (cond
-    (boolean? form)
-    (m.prim/is form)
+  (let [form* (apply-extensions env form)]
+    (cond
+      (reduced? form*)
+      (deref form*)
 
-    (number? form)
-    (m.prim/is form)
+      (not= form* form)
+      (parse env form*)
 
-    (keyword? form)
-    (parse-literal-keyword env form)
+      (boolean? form)
+      (m.prim/is form)
 
-    (string? form)
-    (m.prim/is form)
+      (number? form)
+      (m.prim/is form)
 
-    (symbol? form)
-    (parse-symbol env form)
+      (keyword? form)
+      (parse-literal-keyword env form)
 
-    (seq? form)
-    (parse-seq env form)
+      (string? form)
+      (m.prim/is form)
 
-    (vector? form)
-    (parse-vector env form)
+      (symbol? form)
+      (parse-symbol env form)
 
-    :else
-    (todo env form)))
+      (seq? form)
+      (parse-seq env form)
+
+      (vector? form)
+      (parse-vector env form)
+
+      (map? form)
+      (parse-map env form)
+
+      (set? form)
+      (parse-set env form)
+
+      :else
+      (todo env form))))
 
 
 (comment
+  (comment
+    (define-syntax hash-map-ampersand
+      (m/rule
+       (assoc ?m (m/symbol "meander.zeta" (m/str "&" _)) ?v) 
+       (m/list m/merge ?m ?v))))
+
+  (defn hash-map-ampersand [form]
+    (if (map? form)
+      (if-some [k (some (fn [k]
+                          (if (and (symbol? k)
+                                   (string/starts-with? (name k) "&"))
+                            k))
+                        (keys form))]
+        (let [env (::m.env/env (meta form))]
+          {:object (reduced (m.prim/merge (parse env (dissoc form k)) (parse env (get form k))))}))))
+
+  (defn hash-map-as [form]
+    (if (map? form)
+      (if-some [k (some (fn [k]
+                          (if (and (keyword? k)
+                                   (string/starts-with? (name k) "as"))
+                            k))
+                        (keys form))]
+        (let [env (::m.env/env (meta form))]
+          {:object (reduced (m.prim/each (parse env (get form k)) (parse env (dissoc form k))))}))))
+
   (parse
    (m.env/create {::m.env/operators
-                  {`m.prim/boolean
+                  {`m.prim/rule
+                   (fn [[_ q y :as form]]
+                     (let [env (::m.env/env (meta form))]
+                       {:object (reduced (m.prim/rule (parse env q) (parse env y)))}))
+
+                   `m.prim/boolean
                    (fn [_]
                      {:object `(m.prim/some true false)})
+
                    `m.prim/some
                    (fn [[_ & xs :as form]]
-                     {:object (reduced (apply m.prim/some (parse-all (::m.env (meta form)) xs)))})}})
-   '[1 ~@xs 3 (m.prim/boolean)]))
+                     {:object (reduced (apply m.prim/some (parse-all (::m.env/env (meta form)) xs)))})}
+
+                  ::m.env/extensions
+                  [['hash-map-as hash-map-as]
+                   ['hash-map-ampersand hash-map-ampersand]]})
+   '{:foo 1 :bar 2 & ?x :as ?foo}))
+
+
+
